26475257144a8e51265a1646cd9958dd7c9a7c67
Yuji Sugiura <yuji.sugiura@dena.com>
2015-04-07 14:48:44 +0900

    add code

---
+++ lib/qr-maker.rb
@@ -0,0 +1,17 @@
+require 'thor'
+require 'rqrcode'
+require 'rqrcode_png'
+require 'chunky_png'
+
+class QRMaker < Thor
+    desc 'gen URL DEST -size=SIZE', 'Generate QR by specified size'
+    option :size, :type => :numeric, :aliases => :s, :default => 240
+    def gen(url, dest = './qr.png')
+        size = options[:size]
+
+        qr = RQRCode::QRCode.new(url, :size => 3, :level => :h)
+        png = qr.to_img
+        png.resize(size, size).save(dest)
+        p 'QR code created -> ' + dest
+    end
+end
---
+++ main.rb
@@ -0,0 +1,4 @@
+#!/usr/bin/env ruby
+require './lib/qr-maker'
+
+QRMaker.start
