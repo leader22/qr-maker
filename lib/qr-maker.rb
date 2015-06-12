df72fdb2440f0a167d35613d0207b93aadc4ce0a
Yuji Sugiura <yuji.sugiura@dena.com>
2015-06-12 14:26:03 +0900

    upgrade qr size

--- lib/qr-maker.rb
+++ lib/qr-maker.rb
@@ -9,7 +9,7 @@ class QRMaker < Thor
     def gen(url, dest = './qr.png')
         size = options[:size]
 
-        qr = RQRCode::QRCode.new(url, :size => 3, :level => :h)
+        qr = RQRCode::QRCode.new(url, :size => 5, :level => :h)
         png = qr.to_img
         png.resize(size, size).save(dest)
         p 'QR code created -> ' + dest
